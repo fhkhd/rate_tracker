2db5749b7b6a57d1b679a863b8eb475c933016a5
hanie.khodkari <haniekhodkari@gmail.com>
2024-09-24 18:36:11 +0330

    Set ArticleModel

---
+++ lib/features/news/data/models/article_model.dart
@@ -0,0 +1,29 @@
+import '../../domain/entities/article.dart';
+
+class ArticleModel extends Article {
+  ArticleModel({
+    required super.id,
+    required super.name,
+    required super.author,
+    required super.title,
+    required super.description,
+    required super.url,
+    required super.urlToImage,
+    required super.publishedAt,
+    required super.content,
+  });
+
+  factory ArticleModel.fromJson(Map<String, dynamic> json) {
+    return ArticleModel(
+      id: json['source']['id'],
+      name: json['source']['name'],
+      author: json['author'],
+      title: json['title'],
+      description: json['description'],
+      url: json['url'],
+      urlToImage: json['urlToImage'],
+      publishedAt: json['publishedAt'],
+      content: json['content'],
+    );
+  }
+}
---
+++ lib/features/news/domain/entities/article.dart
@@ -0,0 +1,23 @@
+class Article {
+  final String id;
+  final String name;
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String urlToImage;
+  final String publishedAt;
+  final String content;
+
+  Article({
+    required this.id,
+    required this.name,
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+}

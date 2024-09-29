2948b750220717a96bf0fc13cde874cd7b0f5a7f
hanie.khodkari <haniekhodkari@gmail.com>
2024-09-29 19:38:21 +0330

    Add tween to animation switcher

--- lib/features/news/presentation/widgets/news_item_widget.dart
+++ lib/features/news/presentation/widgets/news_item_widget.dart
@@ -21,67 +21,78 @@ class NewsItemWidget extends StatelessWidget {
       onTap: onTap,
       child: AnimatedSwitcher(
           duration: const Duration(milliseconds: 500),
+          key: ValueKey<String>(article.id),
+          transitionBuilder: (child, animation) {
+            return ScaleTransition(
+              scale: Tween<double>(begin: isSelected ? 0.8 : 1.0, end: 1.0)
+                  .animate(animation),
+              child: child,
+            );
+          },
           child: isSelected
               ? Card(
+                  key: ValueKey<String>(article.id),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(1.w),
-                    child: Column(
-                      children: [
-                        Card(
-                          color:
-                              Theme.of(context).colorScheme.secondaryContainer,
-                          child: article.urlToImage != 'null'
-                              ? ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: Image.network(
-                                    article.urlToImage,
-                                    fit: BoxFit.cover,
-                                    loadingBuilder: (BuildContext context,
-                                        Widget child,
-                                        ImageChunkEvent? loadingProgress) {
-                                      if (loadingProgress == null) return child;
-                                      return Center(
-                                        child: CircularProgressIndicator(
-                                          value: loadingProgress
-                                                      .expectedTotalBytes !=
-                                                  null
-                                              ? loadingProgress
-                                                      .cumulativeBytesLoaded /
-                                                  loadingProgress
-                                                      .expectedTotalBytes!
-                                              : null,
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                )
-                              : Icon(
-                                  Icons.image_outlined,
-                                  size: 20.w,
-                                  color: Theme.of(context).colorScheme.tertiary,
-                                ),
-                        ),
-                        Text(
-                          article.title,
-                          style: Theme.of(context).textTheme.titleMedium,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        Text(
-                          article.name,
-                          style: Theme.of(context).textTheme.labelSmall,
-                        ),
-                        Text(
-                          dateConverter(article.publishedAt),
-                          style: Theme.of(context).textTheme.labelSmall,
-                        ),
-                      ],
+                    side: BorderSide(
+                      width: 2,
+                      color: Theme.of(context).colorScheme.tertiary,
                     ),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        child: article.urlToImage != 'null'
+                            ? ClipRRect(
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10),
+                                ),
+                                child: Image.network(
+                                  article.urlToImage,
+                                  fit: BoxFit.cover,
+                                  loadingBuilder: (BuildContext context,
+                                      Widget child,
+                                      ImageChunkEvent? loadingProgress) {
+                                    if (loadingProgress == null) return child;
+                                    return Center(
+                                      child: CircularProgressIndicator(
+                                        value: loadingProgress
+                                                    .expectedTotalBytes !=
+                                                null
+                                            ? loadingProgress
+                                                    .cumulativeBytesLoaded /
+                                                loadingProgress
+                                                    .expectedTotalBytes!
+                                            : null,
+                                      ),
+                                    );
+                                  },
+                                ),
+                              )
+                            : Icon(
+                                Icons.image_outlined,
+                                size: 50.w,
+                                color: Theme.of(context).colorScheme.tertiary,
+                              ),
+                      ),
+                      Text(
+                        article.title,
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      Text(
+                        article.name,
+                        style: Theme.of(context).textTheme.labelSmall,
+                      ),
+                      Text(
+                        dateConverter(article.publishedAt),
+                        style: Theme.of(context).textTheme.labelSmall,
+                      ),
+                    ],
                   ),
                 )
               : Card(
+                  key: ValueKey<String>("${article.id}_unselected"),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
